d27291b45317899c4cf9bc088e984752f940e2d4
Alex <gurievalex78@gmail.com>
2020-09-22 19:14:45 +0200

    adj

--- index.php
+++ index.php
@@ -1,23 +1,36 @@
 <?php
-include __DIR__ . '/partials/db.php';
+include 'db.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dist/app.css">
     <title>Php-Ajax-Dischi</title>
 </head>
+
 <body>
-    <h1>Dischi</h1>
-    <?php foreach($database as $dato) {?>
-    <ul>
-        <li><?php echo $dato['title'] ?></li>
-        <li><?php echo $dato['author'] ?></li>
-        <li><?php echo $dato['year'] ?></li>
-        <li><?php echo $dato['poster'] ?></li>
-    </ul>
-    <?php } ?>
+    <header>
+        <div class="header-container">
+            <img src="http://pluspng.com/img-png/spotify-logo-png-open-2000.png" alt="">
+        </div>
+    </header>
+
+    <main>
+        <div class="cds-container">
+            <?php foreach ($database as $cd) { ?>
+                <div class="cd">
+                    <img src="<?= $cd['poster']; ?>" />
+                    <h3><?= $cd['title'] ?></h3>
+                    <small><?= $cd['author']; ?></small>
+                    <strong><?= $cd['year']; ?></strong>
+                </div>
+            <?php } ?>
+        </div>
+    </main>
 </body>
+
 </html>
